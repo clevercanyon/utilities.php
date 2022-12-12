f2b64abc015774c0f910a64dd464df83e0d667db
Jason Caldwell <jaswrks@o5p.me>
2022-12-11 23:02:33 -0500

    Update.

---
+++ .prettierrc.cjs
@@ -0,0 +1,21 @@
+/**
+ * Prettier config file.
+ *
+ * Prettier is aware of this config file's location.
+ *
+ * @note CUSTOM EDITS ONLY PLEASE!
+ * @note In the future this file will be updated automatically.
+ * @note Only `<custom:start.../custom:end>` will be preserved below.
+ */
+/* eslint-env es2021, node */
+
+const mc = require('@clevercanyon/js-object-mc');
+const baseConfig = require('./dev/.files/prettier/config.cjs');
+
+/**
+ * Customizations
+ * <custom:start> */
+
+module.exports = mc.merge({}, baseConfig, {});
+
+/** </custom:end> */
---
+++ dev/.files/docs/gitignore.md
@@ -0,0 +1,14 @@
+# `.gitignore` and Related Files
+
+When changing ignore files, please consult the following files:
+
+-   `@clevercanyon/skeleton/.gitignore`
+-   `@clevercanyon/skeleton/.npmignore`
+
+-   `@clevercanyon/skeleton-dev-deps/.gitignore`
+-   `@clevercanyon/skeleton-dev-deps/.npmignore`
+
+Also review `@jaswrks/dotfiles`.
+
+-   `@jaswrks/dotfiles/.gitignore`
+-   `@jaswrks/dotfiles/.config/vs-code/settings.json`
